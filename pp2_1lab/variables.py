ddd16121c1b634b53324ddae4c73ae4a2865de80
Bek_Ancer <bek.mag.00@mail.ru>
2023-02-03 12:22:35 +0600

    red1

--- pp2_1lab/variables.py
+++ pp2_1lab/variables.py
@@ -12,3 +12,39 @@ print(k+j)#there is no difference between '' and ""
 N='wood'
 n='stone'
 print(N + n)#variable names are case sensitive
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"#u can use this variables
+
+#this variables illegal
+#2myvar = "John"
+#my-var = "John"
+#my var = "John"
+
+#multiple var
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+print(x + y + z)
+
+
+x = "awesome"
+def myfunc():
+  print("Python is " + x)
+
+myfunc()
