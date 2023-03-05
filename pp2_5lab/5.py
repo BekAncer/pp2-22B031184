c7baa1dd11a59a727a42834efae0e9640286fab0
Bek_Ancer <89043156+BekAncer@users.noreply.github.com>
2023-03-05 17:38:31 +0600

    Update 5.py

--- pp2_5lab/5.py
+++ pp2_5lab/5.py
@@ -1,6 +1,6 @@
 # Write a Python program that matches a string that has an 'a' followed by anything, ending in 'b'.
 import re 
 s = input() 
-pattern = r'a+.+b$' 
+pattern = r'.?a+.?b\s' 
 ls = re.findall(pattern, s) 
 print(*ls)
