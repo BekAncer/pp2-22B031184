49be22343bb6e36998c1fb4b24f70225739ee8eb
Bek_Ancer <89043156+BekAncer@users.noreply.github.com>
2023-06-17 21:04:31 +0600

    Update classexample.py

--- classexample/classexample.py
+++ classexample/classexample.py
@@ -2,17 +2,29 @@ class User:
     def __init__(self,name):
         self.name=name
 
-class zal:
-    def __init__(self,maxmest):
-        self.maxmest=maxmest
-        self.mesto=[]
-    def __add__(self,other):
+class Zal:
+    def __init__(self, maxmest):
+        self.maxmest = maxmest
+        self.mesto = []
+    def __add__(self, other):
         self.maxmest = self.maxmest + other.maxmest
         self.mesto += other.mesto
-
-    def enter(self,user):
+    def enter(self, user):
         self.mesto.append(user)
-    def checkplace(self,user):
+    def check_place(self, user):
         return self.mesto.index(user)
-    def exit(self,user):
+    def exit(self, user):
         self.mesto.pop(self.mesto.index(user))
+
+
+Tommy = User('Thomas Angelo')
+Jo = User('Joe Barbaro')
+zal1 = zal(20)
+zal1.enter(Tommy.name)
+print(zal1.mesto)
+zal2 = zal(30)
+zal2.enter(Jo.name)
+print(zal2.mesto)
+zal1 + zal2
+print(zal1.maxmest)
+print(zal1.mesto)
