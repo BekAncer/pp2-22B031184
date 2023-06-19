d43b8904c14d48e49ccbab9aaf699a7e82cdbf4e
Bek_Ancer <89043156+BekAncer@users.noreply.github.com>
2023-06-20 01:37:46 +0600

    Update classexample.py

--- classexample/classexample.py
+++ classexample/classexample.py
@@ -1,16 +1,19 @@
 class User:
-    def __init__(self, name):
-        self.name = name
+    def __init__(self,name):
+        self.name=name
 
 class Zal:
-    def __init__(self, maxmest):
+    def __init__(self, maxmest, mesto = []):
         self.maxmest = maxmest
-        self.mesto = []
+        self.mesto = mesto
     def __add__(self, other):
+        return Zal(self.maxmest + other.maxmest,self.mesto + other.mesto)
+    def __iadd__(self, other):
         self.maxmest += other.maxmest
         self.mesto += other.mesto
+        return self
     def enter(self, user):
-        self.mesto.append(user)
+        self.mesto= self.mesto + [user]
     def check_place(self, user):
         return self.mesto.index(user)
     def exit(self, user):
@@ -19,12 +22,24 @@ class Zal:
 
 Tommy = User('Thomas Angelo')
 Jo = User('Joe Barbaro')
+
 zal1 = Zal(20)
-zal1.enter(Tommy.name)
+zal1.enter(Tommy)
 print(zal1.mesto)
+
+
 zal2 = Zal(30)
-zal2.enter(Jo.name)
+zal2.enter(Jo)
 print(zal2.mesto)
-zal1 + zal2
+
+
+zal1 = zal1 + zal2
 print(zal1.maxmest)
 print(zal1.mesto)
+
+
+Clemente = User('Alberto Clemente')
+zal3 = Zal(40)
+zal3.enter(Clemente)
+zal3 += zal2
+print(zal3.check_place(Clemente))
